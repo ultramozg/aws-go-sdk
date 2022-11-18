413907af97ab6720c86a46243a2cf11956438e04
Dmitri Candu <dmitri.candu@planetpayment.com>
2022-11-18 17:24:34 +0200

    Initial work

---
+++ go.mod
@@ -0,0 +1,23 @@
+module github.com/ultramozg/aws-go-sdk/msk
+
+go 1.18
+
+require (
+	github.com/aws/aws-sdk-go-v2 v1.17.1 // indirect
+	github.com/aws/aws-sdk-go-v2/config v1.18.1 // indirect
+	github.com/aws/aws-sdk-go-v2/credentials v1.13.1 // indirect
+	github.com/aws/aws-sdk-go-v2/feature/ec2/imds v1.12.19 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/configsources v1.1.25 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/endpoints/v2 v2.4.19 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/ini v1.3.26 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/presigned-url v1.9.19 // indirect
+	github.com/aws/aws-sdk-go-v2/service/kafka v1.18.0 // indirect
+	github.com/aws/aws-sdk-go-v2/service/sso v1.11.25 // indirect
+	github.com/aws/aws-sdk-go-v2/service/ssooidc v1.13.8 // indirect
+	github.com/aws/aws-sdk-go-v2/service/sts v1.17.3 // indirect
+	github.com/aws/smithy-go v1.13.4 // indirect
+	github.com/klauspost/compress v1.15.9 // indirect
+	github.com/pierrec/lz4/v4 v4.1.15 // indirect
+	github.com/segmentio/kafka-go v0.4.38 // indirect
+	github.com/segmentio/kafka-go/sasl/aws_msk_iam_v2 v0.0.0-20221109011304-a8e74f3adaea // indirect
+)
---
+++ msk/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"context"
+	"crypto/tls"
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/segmentio/kafka-go"
+	"github.com/segmentio/kafka-go/sasl/aws_msk_iam_v2"
+)
+
+func main() {
+	cfg, err := config.LoadDefaultConfig(context.TODO(), config.WithRegion("eu-west-1"))
+	if err != nil {
+		log.Fatalf("unable to load SDK config, %v", err)
+	}
+
+	mechanism := aws_msk_iam_v2.NewMechanism(cfg)
+	r := kafka.NewReader(kafka.ReaderConfig{
+		Brokers:     []string{"b-1-public.devspvdeveuwest1euwest.mv7mz5.c9.kafka.eu-west-1.amazonaws.com:9198", "b-2-public.devspvdeveuwest1euwest.mv7mz5.c9.kafka.eu-west-1.amazonaws.com:9198"},
+		GroupID:     "some-consumer-group",
+		GroupTopics: []string{"some-topic"},
+		Dialer: &kafka.Dialer{
+			Timeout:       10 * time.Second,
+			DualStack:     true,
+			SASLMechanism: mechanism,
+			TLS:           &tls.Config{},
+		},
+	})
+
+	for {
+		m, err := r.ReadMessage(context.Background())
+		if err != nil {
+			log.Fatal(err)
+			break
+		}
+		fmt.Printf("message at offset %d: %s = %s\n", m.Offset, string(m.Key), string(m.Value))
+	}
+
+	if err := r.Close(); err != nil {
+		log.Fatal("failed to close reader:", err)
+	}
+}
