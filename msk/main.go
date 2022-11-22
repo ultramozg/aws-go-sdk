c75f4b15866d40938b7609f945e235d5cc73c8e8
Dmitri Candu <dmitri.candu@planetpayment.com>
2022-11-22 15:56:19 +0200

    POC ACL + update connectivity info

--- msk/main.go
+++ msk/main.go
@@ -9,7 +9,7 @@ import (
 
 	"github.com/aws/aws-sdk-go-v2/config"
 	msk "github.com/aws/aws-sdk-go-v2/service/kafka"
-	types "github.com/aws/aws-sdk-go-v2/service/kafka/types"
+	"github.com/aws/aws-sdk-go-v2/service/kafka/types"
 	"github.com/segmentio/kafka-go"
 	"github.com/segmentio/kafka-go/sasl/aws_msk_iam_v2"
 )
@@ -24,6 +24,7 @@ func main() {
 	resp, err := svc.ListClustersV2(context.TODO(), &msk.ListClustersV2Input{})
 	fmt.Println(resp)
 
+	//==================  Update ACL for the kafka ================
 	mechanism := aws_msk_iam_v2.NewMechanism(cfg)
 	transport := &kafka.Transport{
 		SASL: mechanism,
@@ -96,13 +97,15 @@ func main() {
 	}
 
 	//========= Make cluster public ===========
-	// TODO need to fetch the cluster version: K3DWYIK6Y9EEQB
 	arn := "arn:aws:kafka:eu-west-1:748861314284:cluster/poc-region1/96167cca-e109-43c8-a983-1e779e93f993-9"
-	curVersion := "K3DWYIK6Y9EEQB"
 	pub := "SERVICE_PROVIDED_EIPS"
+	info, err := svc.DescribeCluster(context.TODO(), &msk.DescribeClusterInput{ClusterArn: &arn})
+	if err != nil {
+		log.Fatal(err)
+	}
 	status, err := svc.UpdateConnectivity(context.TODO(), &msk.UpdateConnectivityInput{
 		ClusterArn:     &arn,
-		CurrentVersion: &curVersion,
+		CurrentVersion: info.ClusterInfo.CurrentVersion,
 		ConnectivityInfo: &types.ConnectivityInfo{
 			PublicAccess: &types.PublicAccess{
 				Type: &pub,
@@ -113,4 +116,20 @@ func main() {
 		log.Fatal(err)
 	}
 	fmt.Println(status)
+
+	// Wait unit the cluster will be public
+	for {
+		data, err := svc.GetBootstrapBrokers(context.TODO(), &msk.GetBootstrapBrokersInput{
+			ClusterArn: &arn,
+		})
+		if err != nil {
+			log.Fatal(err)
+		}
+		if data.BootstrapBrokerStringPublicSaslIam != nil {
+			fmt.Println("INFO: ", *data.BootstrapBrokerStringPublicSaslIam)
+			break
+		}
+		fmt.Println("MSK is updating")
+		time.Sleep(60 * time.Second)
+	}
 }
